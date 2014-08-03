2fd62c42584ea82d49e3b6ec336028855578cb65
Samuel Deléglise <samuel.deleglise@gmail.com>
2014-08-03 23:39:18 +0200

    single exe file works

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from distutils.core import setup
 import py2exe
 
-setup(windows=[{"script":"subviewer.py"}], options={"py2exe":{"includes":["sip"]}})
+setup(windows=[{"script":"subviewer.py"}],
+      options={"py2exe":{"includes":["sip"],
+                         'bundle_files': 1,
+                         'compressed': True}},
+      zipfile = None)
